3d075766f6a1f7d0ad72837457504d672377aa1d
Anthony Grant <70977450+anthonygc@users.noreply.github.com>
2023-03-21 10:21:01 -0400

    Correct the hitbox functionality

--- JabReality/Assets/Scripts/gloveHit.cs
+++ JabReality/Assets/Scripts/gloveHit.cs
@@ -18,10 +18,7 @@ public class gloveHit : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(transform.position,transform.forward,out hit, 1, layer))
         {
-            if (Vector3.Angle(transform.position-previousPos,hit.transform.up)> 130)
-            {
-                Destroy(hit.transform.gameObject);
-            }
+            Destroy(hit.transform.gameObject);
         }
         previousPos = transform.position;
     }
